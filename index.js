26275570870e4c0cf80945119c9c51c02a3b6cc6
Dan Lynch <pyramation@gmail.com>
2015-11-19 19:16:17 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+var codes = [
+    200,
+    200,
+    200,
+    500,
+    502,
+    503,
+    504
+];
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomCode() {
+    return codes[randInt(0, codes.length-1)];
+}
+
+var body = require('./lib/ipsum');
+
+app.get('/', function (req, res) {
+    var code = randomCode();
+    if (code === 200) {
+      res.header('Content-Type', 'text/html');
+      res.send(body());
+    } else {
+      res.status(code).end();
+    }
+});
+app.get('/style.css', function (req, res) {
+  res.sendFile(__dirname + '/style.css');
+});
+var server = app.listen(7777, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('listening at http://%s:%s', host, port);
+});
